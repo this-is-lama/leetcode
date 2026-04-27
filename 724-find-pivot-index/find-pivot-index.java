0b705b52787b2623a8c19faf37b853fab0981383
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-04-27 21:30:58 +0300

    Time: 23 ms (8.90%) | Memory: 47.5 MB (23.50%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+		int[] prefixSum = new int[nums.length];
+		prefixSum[0] = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			prefixSum[i] = nums[i] + prefixSum[i - 1];
+		}
+		System.out.println(Arrays.toString(prefixSum));
+		int[] suffixSum = new int[nums.length];
+		suffixSum[nums.length - 1] = nums[nums.length - 1];
+		for (int i = nums.length - 2; i >= 0; i--) {
+			suffixSum[i] = suffixSum[i + 1] + nums[i];
+		}
+		System.out.println(Arrays.toString(suffixSum));
+		for (int i = 0; i < nums.length; i++) {
+			if (prefixSum[i] == suffixSum[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
+}
