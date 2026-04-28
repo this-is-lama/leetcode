b98ba5165d84a9aef359852dd94d261b55842825
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-04-28 18:56:26 +0300

    Time: 8 ms (97.37%) | Memory: 49.7 MB (96.42%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,17 +1,16 @@
 class Solution {
     public int equalPairs(int[][] grid) {
 		int n = grid.length;
+		int[][] transpose = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				transpose[j][i] = grid[i][j];
+			}
+		}
 		int count = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				boolean equal = true;
-				for (int k = 0; k < n; k++) {
-					if (grid[i][k] != grid[k][j]) {
-						equal = false;
-						break;
-					}
-				}
-				if (equal) {
+				if (Arrays.equals(grid[i], transpose[j])) {
 					count++;
 				}
 			}
