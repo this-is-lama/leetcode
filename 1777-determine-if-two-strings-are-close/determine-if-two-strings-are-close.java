24e4d9f3ef4dd164f0934de724c7aba6ffd360cc
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-04-28 18:22:04 +0300

    Time: 11 ms (84.64%) | Memory: 47.7 MB (94.45%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -11,7 +11,7 @@ class Solution {
 		}
 
 		for (int i = 0; i < 26; i++) {
-			if (freq1[i] > 0 && freq2[i] == 0 || freq2[i] > 0 && freq1[i] == 0) {
+			if ((freq1[i] > 0 && freq2[i] == 0) || (freq2[i] > 0 && freq1[i] == 0)) {
 				return false;
 			}
 		}
