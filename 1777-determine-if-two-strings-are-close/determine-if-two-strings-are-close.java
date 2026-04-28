6e12c0d066a669106ce8cea6d2385ce85f3698e4
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-04-28 18:20:42 +0300

    Time: 11 ms (84.64%) | Memory: 48.1 MB (39.93%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
+		if (word1.length() != word2.length()) return false;
 		int[] freq1 = new int[26];
 		int[] freq2 = new int[26];
 		for (char c : word1.toCharArray()) {
