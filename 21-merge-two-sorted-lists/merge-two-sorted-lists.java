e76c4785395d34ae5817a04e9cca1731d0752218
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-08-21 22:53:32 +0300

    Time: 0 ms (100.00%) | Memory: 44.4 MB (39.01%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -12,52 +12,43 @@ class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 		if (list1 == null) return list2;
 		if (list2 == null) return list1;
-
-
+		
 		ListNode curr1 = list1;
 		ListNode curr2 = list2;
-		ListNode head = null;
-		ListNode curr = null;
-
+		ListNode head;
+		ListNode curr;
+		
+		if (curr1.val < curr2.val) {
+			head = curr1;
+			curr = head;
+			curr1 = curr1.next;
+		} else {
+			head = curr2;
+			curr = head;
+			curr2 = curr2.next;
+		}
+		
 		while (curr1 != null && curr2 != null) {
 			if (curr1.val < curr2.val) {
-				if (head == null) {
-					head = curr1;
-					curr = head;
-				} else {
-					curr.next = curr1;
-					curr = curr.next;
-				}
-				curr1 = curr1.next;
-			} else {
-				if (head == null) {
-					head = curr2;
-					curr = head;
-				} else {
-					curr.next = curr2;
-					curr = curr.next;
-				}
-				curr2 = curr2.next;
-			}
-		}
-		while (curr1 != null) {
-			if (head == null) {
-				head = curr1;
-				curr = head;
-			} else {
 				curr.next = curr1;
 				curr = curr.next;
-			}
-			curr1 = curr1.next;
-		}
-		while (curr2 != null) {
-			if (head == null) {
-				head = curr2;
-				curr = head;
+				curr1 = curr1.next;
 			} else {
 				curr.next = curr2;
 				curr = curr.next;
+				curr2 = curr2.next;
 			}
+		}
+		
+		while (curr1 != null) {
+			curr.next = curr1;
+			curr = curr.next;
+			curr1 = curr1.next;
+		}
+		
+		while (curr2 != null) {
+			curr.next = curr2;
+			curr = curr.next;
 			curr2 = curr2.next;
 		}
 
