a51d4e28cbb6f11da1f81fd60080f81205059de1
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-08-21 21:39:18 +0300

    Time: 14 ms (9.83%) | Memory: 110.1 MB (5.32%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,28 +9,26 @@
  * }
  */
 class Solution {
-    private ListNode left;
-
-public boolean isPalindrome(ListNode head) {
-    left = head;
-    return check(head);
-}
-
-private boolean check(ListNode right) {
-    if (right == null) {
-        return true;
-    }
-
-    if (!check(right.next)) {
-        return false;
-    }
-
-    if (left.val != right.val) {
-        return false;
-    }
-
-    left = left.next;
-
-    return true;
-}
+    public boolean isPalindrome(ListNode head) {
+		if (head == null || head.next == null) return true;
+		
+		List<Integer> list = new java.util.LinkedList<>();
+		ListNode curr = head;
+		while (curr != null) {
+			list.add(curr.val);
+			curr = curr.next;
+		}
+		
+		while (!list.isEmpty()) {
+			if (!Objects.equals(list.getFirst(), list.getLast())) {
+				return false;
+			}
+			list.removeFirst();
+			if (!list.isEmpty()) {
+				list.removeLast();
+			}
+		}
+		
+		return true;
+	}
 }
