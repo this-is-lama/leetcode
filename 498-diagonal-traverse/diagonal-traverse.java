aad982b9d165a75267981e34d9c5d07d4a947607
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-08-23 15:21:11 +0300

    Time: 2 ms (98.76%) | Memory: 48.3 MB (32.84%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -4,50 +4,36 @@ class Solution {
 		int m = mat.length;
 		int n = mat[0].length;
 		int[] result = new int[n * m];
-		int[] position = new int[] {0, 0, 0};
-		while (position[0] <= m && position[1] <= n) {
-			up(position, mat, result);
-			down(position, mat, result);
+		int i = 0;
+		int j = 0;
+		int index = 0;
+		while (i <= m && j <= n) {
+			while (i >= 0 && j < n) {
+				result[index++] = mat[i][j];
+				i--;
+				j++;
+			}
+			i++;
+			j--;
+			if (j == n - 1) {
+				i++;
+			} else {
+				j++;
+			}
+			while (i < m && j >= 0) {
+				result[index++] = mat[i][j];
+				i++;
+				j--;
+			}
+			i--;
+			j++;
+			if (i == m - 1) {
+				j++;
+			} else {
+				i++;
+			}
+
 		}
 		return result;
 	}
-	public void up (int[] position, int[][] mat, int[] result) {
-		int n = mat[0].length;
-		int i = position[0];
-		int j = position[1];
-		while (i >= 0 && j < n) {
-			result[position[2]++] = mat[i][j];
-			i--;
-			j++;
-		}
-		i++;
-		j--;
-		if (j == n - 1) {
-			i++;
-		} else {
-			j++;
-		}
-		position[0] = i;
-		position[1] = j;
-	}
-	public void down (int[] position, int[][] mat, int[] result) {
-		int m = mat.length;
-		int i = position[0];
-		int j = position[1];
-		while (i < m && j >= 0) {
-			result[position[2]++] = mat[i][j];
-			i++;
-			j--;
-		}
-		i--;
-		j++;
-		if (i == m - 1) {
-			j++;
-		} else {
-			i++;
-		}
-		position[0] = i;
-		position[1] = j;
-	}
-
 }
