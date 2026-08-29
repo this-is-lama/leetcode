15c5ca542f6872e4f6dd0d40f5d56ae3f898a778
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-08-29 17:51:28 +0300

    Time: 2 ms (46.54%) | Memory: 44.1 MB (10.29%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,38 +1,28 @@
 class Solution {
     
     public static String addBinary(String a, String b) {
-		StringBuilder result = new StringBuilder();
-		char[] main;
-		char[] other;
+		StringBuilder builder = new StringBuilder();
 
-		if (a.length() >= b.length()) {
-			main = a.toCharArray();
-			other = b.toCharArray();
-		} else {
-			main = b.toCharArray();
-			other = a.toCharArray();
-		}
-		int mainPointer = main.length - 1;
-		int otherPointer = other.length - 1;
+		int i = a.length() - 1;
+		int j = b.length() - 1;
 
-		int ostatok = 0;
-		while (otherPointer >= 0) {
-			int num = (main[mainPointer] - '0' + other[otherPointer] - '0' + ostatok);
-			ostatok = num / 2;
-			result.insert(0, Character.toString(num % 2 + '0'));
-			otherPointer--;
-			mainPointer--;
-		}
-		while (mainPointer >= 0) {
-			int num = (main[mainPointer] + ostatok - '0');
-			ostatok = num / 2;
-			result.insert(0, Character.toString(num % 2 + '0'));
-			mainPointer--;
-		}
-		if (ostatok == 1) {
-			result.insert(0, '1');
-		}
+		int carry = 0;
+		while (i >= 0 || j >= 0 || carry > 0) {
+			int sum = carry;
 
-		return result.toString();
+			if (i >= 0) {
+				sum += a.charAt(i) - '0';
+				i--;
+			}
+			if  (j >= 0) {
+				sum += b.charAt(j) - '0';
+				j--;
+			}
+
+			builder.insert(0, sum % 2);
+			carry = sum / 2;
+		}
+		
+		return builder.toString();
 	}
 }
