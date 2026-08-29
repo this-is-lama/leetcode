cf9c29ff6c4251f3df368a98092cb2a47cb28b0a
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-08-29 16:15:27 +0300

    Time: 2 ms (11.14%) | Memory: 43.2 MB (93.95%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -5,25 +5,23 @@ class Solution {
 
 		for (int level = 0; level < numRows; level++) {
 			List<Integer> row = new ArrayList<>();
-			switch (level) {
-				case 0 -> row.add(1);
-				case 1 -> {
-					row.add(1);
-					row.add(1);
+			if (level == 0) {
+				row.add(1);
+			} else if (level == 1) {
+				row.add(1);
+				row.add(1);
+			} else {
+				var prev = result.getLast();
+				row.add(1);
+				for (int i = 0; i < prev.size() - 1; i++) {
+					row.add(prev.get(i) + prev.get(i + 1));
 				}
-				default -> {
-					var prev = result.getLast();
-					row.add(1);
-					for (int i = 0; i < prev.size() - 1; i++) {
-						row.add(prev.get(i) + prev.get(i + 1));
-					}
-					row.add(1);
-				}
+				row.add(1);
 			}
 			
 			result.add(row);
 		}
-        
+
 		return result;
 	}
 }
