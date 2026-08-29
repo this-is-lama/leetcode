9d58748542632ed176cfb4e0853ba538fab7d948
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-08-29 16:15:00 +0300

    Time: 2 ms (11.14%) | Memory: 43.7 MB (16.22%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,30 +1,29 @@
 class Solution {
 
     public static List<List<Integer>> generate(int numRows) {
-		List<List<Integer>> result = new ArrayList<>();
-		
-		for (int i = 0; i < numRows; i++) {
+		List<List<Integer>> result = new LinkedList<>();
+
+		for (int level = 0; level < numRows; level++) {
 			List<Integer> row = new ArrayList<>();
-			if (i == 0) {
-				row.add(1);
-				result.add(row);
-				continue;
-			} 
-			if (i == 1) {
-				row.add(1);
-                row.add(1);
-				result.add(row);
-				continue;
+			switch (level) {
+				case 0 -> row.add(1);
+				case 1 -> {
+					row.add(1);
+					row.add(1);
+				}
+				default -> {
+					var prev = result.getLast();
+					row.add(1);
+					for (int i = 0; i < prev.size() - 1; i++) {
+						row.add(prev.get(i) + prev.get(i + 1));
+					}
+					row.add(1);
+				}
 			}
-			var prev = result.get(i - 1);
-			row.add(1);
-			for (int index = 0; index < prev.size() - 1; index++) {
-				row.add(prev.get(index) + prev.get(index + 1));
-			}
-			row.add(1);
-			result.add(row);
 			
+			result.add(row);
 		}
+        
 		return result;
 	}
 }
