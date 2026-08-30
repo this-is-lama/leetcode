e9049174aa32b9a2ea863be35b1933bc1e8b79ab
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-08-30 14:48:05 +0300

    Time: 1 ms (70.36%) | Memory: 42.5 MB (55.24%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,25 +1,21 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-		List<List<Integer>> result = new LinkedList<>();
+		List<Integer> list = new ArrayList<>();
 
 		for (int level = 0; level <= rowIndex; level++) {
-			List<Integer> row = new ArrayList<>();
-			if (level == 0) {
-				row.add(1);
-			} else if (level == 1) {
-				row.add(1);
-				row.add(1);
+			if (level < 2) {
+				list.add(1);
 			} else {
-				var prev = result.getLast();
-				row.add(1);
-				for (int i = 0; i < prev.size() - 1; i++) {
-					row.add(prev.get(i) + prev.get(i + 1));
+				List<Integer> newList = new ArrayList<>();
+				newList.add(1);
+				for (int i = 0; i < list.size() - 1; i++) {
+					newList.add(list.get(i) + list.get(i + 1));
 				}
-				row.add(1);
+				newList.add(1);
+				list = newList;
 			}
-
-			result.add(row);
 		}
-		return result.get(rowIndex);
+
+		return list;
 	}
 }
