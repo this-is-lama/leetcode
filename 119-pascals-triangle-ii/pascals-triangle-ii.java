1ce2dcd2da0bc4869b7af532915582255cafce97
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-09-03 11:15:52 +0300

    Time: 1 ms (69.89%) | Memory: 42.8 MB (18.67%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,21 +1,23 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-		List<Integer> list = new ArrayList<>();
+		int[][] cache = new int[rowIndex + 1][rowIndex + 1];
+		List<Integer> row = new LinkedList<>();
 
-		for (int level = 0; level <= rowIndex; level++) {
-			if (level < 2) {
-				list.add(1);
-			} else {
-				List<Integer> newList = new ArrayList<>();
-				newList.add(1);
-				for (int i = 0; i < list.size() - 1; i++) {
-					newList.add(list.get(i) + list.get(i + 1));
-				}
-				newList.add(1);
-				list = newList;
-			}
+		for (int columnIndex = 0; columnIndex <= rowIndex; columnIndex++) {
+			row.add(element(rowIndex, columnIndex, cache));
 		}
 
-		return list;
+		return row;
+	}
+
+	public int element(int row, int column, int[][] cache) {
+		if (column == 0 || row == column) return 1;
+		if (cache[row][column] != 0) {
+			return cache[row][column];
+		}
+		
+		int result = element(row - 1, column - 1, cache) + element(row - 1 , column, cache);
+		cache[row][column] = result;
+		return result;
 	}
 }
