37385352a6bcd352eb997cfdf5d57e82c9ba5d6c
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-09-10 19:26:58 +0300

    Time: 12 ms (99.25%) | Memory: 109 MB (13.43%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -2,10 +2,10 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
 		Set<Integer> set = new HashSet<>();
 		for (int i : nums) {
-			if (set.contains(i)) {
+			if (!set.add(i)) {
 				return true;
 			}
-			set.add(i);
+			
 		}
 		return false;
 	}
