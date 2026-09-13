3a0b7251e358d4ea78b9dd8ece45aaaf920bde20
Islam Ahmatgaliev <islamahmatgaliev0354@gmail.com>
2026-09-13 14:01:03 +0300

    Time: 12 ms (55.99%) | Memory: 44.3 MB (9.92%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
 		Map<Character, Character> compliance = new HashMap<>();
+		Map<Character, Character> isomorphic = new HashMap<>();
 
 		for (int i = 0; i < s.length(); i++) {
 			if (compliance.containsKey(s.charAt(i))) {
@@ -8,11 +9,14 @@ class Solution {
 				if (ch != t.charAt(i)) {
 					return false;
 				}
-			} else {
-				if (compliance.containsValue(t.charAt(i))) {
+			} else if (isomorphic.containsKey(t.charAt(i))) {
+				char ch = isomorphic.get(t.charAt(i));
+				if (ch != s.charAt(i)) {
 					return false;
 				}
+			} else {
 				compliance.put(s.charAt(i), t.charAt(i));
+				isomorphic.put(t.charAt(i), s.charAt(i));
 			}
 		}
 
